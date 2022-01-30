536cab09dc170b5826edff5a22a3b707bd625d64
Engines-screaming <karl.ravago@gmail.com>
2022-01-29 20:10:48 -0800

    added url validator to photo url field

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SelectField
-from wtforms.validators import InputRequired, Optional, NumberRange
+from wtforms.validators import InputRequired, Optional, NumberRange, URL
 
 
 class AddPet(FlaskForm):
@@ -9,7 +9,7 @@ class AddPet(FlaskForm):
     name = StringField('Pet Name', validators=[InputRequired()])
     species = SelectField('Species', choices=['dog', 'cat', 'porcupine'], 
                             validators=[InputRequired()])
-    photo_url = StringField('Photo URL', validators=[Optional()])
+    photo_url = StringField('Photo URL', validators=[Optional(), URL()])
     age = IntegerField('Age', validators=[Optional(), NumberRange(0, 30)])
     notes = StringField('Notes', validators=[Optional()])
     
