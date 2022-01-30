87cdcc4f2be3be5475321edfcb66fb9f329c4c7b
Engines-screaming <karl.ravago@gmail.com>
2022-01-29 20:07:57 -0800

    added new validators to add pet form

--- forms.py
+++ forms.py
@@ -1,14 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
-from wtforms.validators import InputRequired, Optional
+from wtforms import StringField, IntegerField, SelectField
+from wtforms.validators import InputRequired, Optional, NumberRange
 
 
 class AddPet(FlaskForm):
     '''Form for creating pets'''
 
     name = StringField('Pet Name', validators=[InputRequired()])
-    species = StringField('Species', validators=[InputRequired()])
+    species = SelectField('Species', choices=['dog', 'cat', 'porcupine'], 
+                            validators=[InputRequired()])
     photo_url = StringField('Photo URL', validators=[Optional()])
-    age = IntegerField('Age', validators=[Optional()])
+    age = IntegerField('Age', validators=[Optional(), NumberRange(0, 30)])
     notes = StringField('Notes', validators=[Optional()])
     
